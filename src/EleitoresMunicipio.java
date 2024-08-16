e40a179a828bc3e847720d5fbe1a5e3a453d50c0
Samuel Prado <samuelprado.99@hotmail.com>
2024-08-15 21:30:41 -0300

    Começo do exercicio 2

---
+++ src/EleitoresMunicipio.java
@@ -0,0 +1,21 @@
+public class EleitoresMunicipio {
+    private int votos;
+    private int branco;
+    private int nulo;
+    private int valido;
+    
+    public EleitoresMunicipio(double votos, double branco, double nulo, double valido){
+     this.votos = votos;
+     this.branco = branco;
+     this.nulo = nulo;
+     this.valido = valido;   
+    }
+   
+
+
+
+
+
+
+
+}
